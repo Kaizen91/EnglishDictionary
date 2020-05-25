db6047bc50ebe8193a75677db9536f00e5dae4ad
Stephen Lang <stephenlang91@gmail.com>
2020-05-24 21:39:03 -0400

    touching up edge cases

--- dictionary.py
+++ dictionary.py
@@ -11,7 +11,11 @@ data = json.load(open('data.json'))
 def translate(word):
     word = word.lower()
     if word in data:
-        return data[word.lower()]    
+        return data[word.lower()]
+    elif word.title() in data:
+        return data[word.title()]
+    elif word.upper() in data:
+        return data[word.upper()]    
     elif len(get_close_matches(word, data.keys())) > 0:
         yn =  input('Did you mean %s? \nEnter Y for Yes or N for No.\n' % get_close_matches(word, data.keys())[0])
         if yn == 'Y':
@@ -24,7 +28,6 @@ def translate(word):
         return 'Your word is not in the dictionary. Please try again.'
         
 word = input('Please input a word: ')
-
 output = translate(word)
 
 if type(output) == list:
